5057483fa011f693585cc3a1f1371623fd5fbcbb
Angela Barber <147751213+angelabarber@users.noreply.github.com>
2024-07-15 17:00:51 -0500

    display message and users' guess

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("How's about guessing a number instead of biting me...");
+
+string response = Console.ReadLine();
+
+Console.WriteLine($"Thanks for guessing {response}");
